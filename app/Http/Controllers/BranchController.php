d33550f64ccaa2bcc52076fc90d1a90d64bd6cee
NicolasMenesesB <NicolasMenesesB>
2022-06-11 16:27:14 -0400

    Controller Branch

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Branch;
+use DB;
 
 class BranchController extends Controller
 {
