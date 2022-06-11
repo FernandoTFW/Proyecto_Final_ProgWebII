bc1bc20072bd32c1af6edede0e2c38dce2900f8b
NicolasMenesesB <NicolasMenesesB>
2022-06-11 13:31:27 -0400

    Controllador Client y Product Listos

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Client;
+use DB;
 
 class ClientController extends Controller
 {
-    //
+    public function getClients() {
+        return Client::all();
+    }
+
+    public function listClients() {
+        return view("clients.list", ["clients" => Client::all()]);
+    }
+
+    public function newClient() {
+        return view("clients.new");
+    }
+
+    public function postClient(Request $request) {
+        Client::create([
+            "businessName" => $request->businessName,
+            "nit" => $request->nit
+        ]);
+        return view("clients.list", ["clients" => Client::all()]);
+    }
+
+    public function editClient($id) {
+        $client = Client::where(["id" => $id])->first();
+        return view("clients.edit", ["client" => $client]);
+    }
+
+    public function putClient(Request $request) {
+        $client = Client::where(["id" => $request->id])->first();
+        $client->businessName = $request->businessName;
+        $client->nit = $request->nit;
+        $client->update();
+        return view("clients.list", ["clients" => Client::all()]);
+    }
+
+    public function deleteClient(Request $request) {
+        $client = Client::where(["id" => $request->id])->first();
+        $client->delete();
+        return view("clients.list", ["clients" => Client::all()]);
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Product;
+use DB;
 
 class ProductController extends Controller
 {
-    //
+    public function getProducts() {
+        return Product::all();
+    }
+
+    public function listProducts() {
+        return view("products.list", ["products" => Product::all()]);
+    }
+
+    public function newProduct() {
+        return view("products.new");
+    }
+
+    public function postProduct(Request $request) {
+        Product::create([
+            "productName" => $request->productName,
+            "productPrice" => $request->productPrice,
+            "stock" => $request->stock,
+            "netContent" => $request->netContent
+        ]);
+        return view("products.list", ["products" => Product::all()]);
+    }
+
+    public function editProduct($id) {
+        $product = Product::where(["id" => $id])->first();
+        return view("products.edit", ["product" => $product]);
+    }
+
+    public function putProduct(Request $request) {
+        $product = Product::where(["id" => $request->id])->first();
+        $product->productName = $request->productName;
+        $product->productPrice = $request->productPrice;
+        $product->stock = $request->stock;
+        $product->netContent = $request->netContent;
+        $product->update();
+        return view("products.list", ["products" => Product::all()]);
+    }
+
+    public function deleteProduct(Request $request) {
+        $product = Product::where(["id" => $request->id])->first();
+        $product->delete();
+        return view("products.list", ["products" => Product::all()]);
+    }
 }
